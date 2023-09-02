c53ff6a537e6b061fb722a32b392cc1ba8ca9b20
Ahmed Shariff <shariff.mfa@outlook.com>
2023-09-01 23:42:14 -0700

    Update test files
    remove evaluate-with-file-functions.el
    update test-org-roam-ql with licence and deps to allow gh action to complete

--- tests/test-org-roam-ql.el
+++ tests/test-org-roam-ql.el
@@ -1,7 +1,29 @@
 ;;; org-roam-ql.el --- Tests for org-roam-ql -*- lexical-binding: t -*-
 
+;; Copyright (C) 2023 Shariff AM Faleel
+
+;; Author: Shariff AM Faleel
+;; Package-Requires: ((buttercup))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
 ;; Setup sandbox directory (named sandbox) with: `make sandbox=sandbox v=vvv init'
 ;; Run tests with: `make sandbox=sandbox quick`
+
+;;; Code:
 
 (require 'buttercup)
 (require 'org-roam-ql)
