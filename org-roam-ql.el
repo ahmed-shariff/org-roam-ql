93e191b5640e104f1ff1a23bbbab3acb6d3e7c62
Shariff Faleel <shariff.mfa@outlook.com>
2025-05-02 19:49:07 -0700

    Fix expand-s-like-function not respecting exact with regexp

--- org-roam-ql.el
+++ org-roam-ql.el
@@ -694,7 +694,7 @@ or fallback to using predicate function if `use-regexp' is used."
     (if use-regexp
         (org-roam-ql--process-comparison-function extraction-function
                                                   #'org-roam-ql--predicate-s-match
-                                                  (list match-string))
+                                                  (list match-string exact))
       `([:select id :from nodes :where (like ,slot-name-symbol $s1)]
         ,(if exact
              match-string
